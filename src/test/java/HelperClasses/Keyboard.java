5018e13ae89a5b4c9ee4d1245cf0613611b420f9
Marius-Aurelian Patru <Marius.Patru@ibm.com>
2018-06-05 14:32:12 +0300

    ex1 tema 4

--- src/test/java/HelperClasses/Keyboard.java
+++
@@ -1,8 +0,0 @@
-package HelperClasses;
-
-public interface Keyboard {
-
-    void sendKeys(CharSequence... keysToSend);
-    void pressKey(CharSequence keyToPress);
-    void releaseKey(CharSequence keyToRelease);
-}
